3425498b623e7643dfd458c9f7c41267ee257b6a
astudilloalex <astudilloalex1@gmail.com>
2022-09-19 17:16:56 -0500

    Initial commit

---
+++ CommonCRUD/Enums/Direction.cs
@@ -0,0 +1,17 @@
+﻿namespace CommonCRUD.Enums
+{
+    /// <summary>
+    /// Order directions when query to database.
+    /// </summary>
+    public enum Direction
+    {
+        /// <summary>
+        /// Ascending order.
+        /// </summary>
+        Asc,
+        /// <summary>
+        /// Descending order.
+        /// </summary>
+        Desc,
+    }
+}
